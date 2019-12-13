4e32442e54217e5e83ef1791395a3d26d4392395
tignear <tignear+m@gmail.com>
2019-12-13 22:47:35 +0900

    fix

--- src/services/query/relying-party.ts
+++ src/services/query/relying-party.ts
@@ -1,4 +1,4 @@
-import { RelyingParty } from "../../models/auth/relying-party";
+import { RelyingParty, RelyingPartyRedirectUri } from "../../models/auth/relying-party";
 import { Repository } from "typeorm";
 import ServiceError from "../error";
 import { Service } from "typedi";
@@ -6,9 +6,9 @@ import { Service } from "typedi";
 @Service()
 export default class RelyingPartyQueryService{
     async checkIdAndRedirectUri(repo:Repository<RelyingParty>,rpdbid:string,redirectUri:string):Promise<boolean>{
-        if (await repo.createQueryBuilder()//TODO
-            .innerJoin("rp.redirectUri", "redirectUri")
-            .where("rp.dbId= :id AND redirectUri= :redirectUri", {
+        if (await repo.createQueryBuilder("rp")//TODO
+            .innerJoin(RelyingPartyRedirectUri,"redirectUri")
+            .where("rp.dbId= :id AND rp.redirectUri= :redirectUri", {
                 id: rpdbid,
                 redirectUri: redirectUri,
             }).getCount() === 0
